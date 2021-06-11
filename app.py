1965ac9a89e42cf3edafed48bdbd336af550ce4f
Rizwan-Ahmad786 <65743186+Rizwan-Ahmad786@users.noreply.github.com>
2021-06-11 23:55:32 +0500

    Intial deploy with postgresql

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ from wtforms.validators import InputRequired, Email, Length, DataRequired, Equal
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager , login_manager, UserMixin, login_user, login_required, logout_user, current_user
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Hi_this_is_my_todo_task_app!'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 ENV = 'prod'
 
 if ENV == 'dev':
@@ -16,7 +18,7 @@ if ENV == 'dev':
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:rizwan786@localhost/db_postgres'
 else:
     app.debug=False
-    app.config['DATABASE_URI'] = 'postgres://qjbgrgemhxpznn:4c2a1c3cd4933ab3fe29bb20fa6d06819f7fd69d8d2d312e00bb3d11e2736d69@ec2-34-193-112-164.compute-1.amazonaws.com:5432/d89v8ls5o6h015'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://qjbgrgemhxpznn:4c2a1c3cd4933ab3fe29bb20fa6d06819f7fd69d8d2d312e00bb3d11e2736d69@ec2-34-193-112-164.compute-1.amazonaws.com:5432/d89v8ls5o6h015'
     
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 Bootstrap(app)
@@ -27,6 +29,7 @@ login_manager.login_view = 'login'
 
 
 class User(UserMixin, db.Model):
+    __tablename__='user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(200), index=True, unique=True)
     email = db.Column(db.String(200), index=True, unique=True)
@@ -38,6 +41,7 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class Todo(db.Model): 
+    __tablename__='todo'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200))
     des = db.Column(db.String(500))
