f0087cdae22c25d17237ffcbacfc6a97d3d06f2e
Rizwan-Ahmad786 <65743186+Rizwan-Ahmad786@users.noreply.github.com>
2021-06-27 17:05:01 +0500

    checkbox work fine

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Hi_this_is_my_todo_task_app!'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 
-ENV = 'local'
+ENV = 'post'
 
 if ENV == 'local':
     app.debug = True
